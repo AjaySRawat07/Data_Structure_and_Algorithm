cbd6e3dbdba4cf96be193f63e8fccc6835c532ca
AjaySRawat07 <ajju246455@gmail.com>
2025-07-29 23:35:02 +0530

    pattern_5_6_7

--- 02_Patterns/Pattern 6.java
+++ 02_Patterns/Pattern 6.java
@@ -8,26 +8,26 @@ class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         
-        int space = n / 2;
-        int star =  1;
+        int sp = 1 , star = n/2 + 1;
         
-        for(int i = 1; i <= n; i++){
-            
-            for(int k=1; k <= space; k++){
-                    System.out.print("\t");
-                }
-                
-            for(int j=1; j <= star; j++){
+        for(int i=1; i <= n; i++){
+            for(int k = 1 ; k <= star; k++){
                     System.out.print("*\t");
-                }
-                
+            }
+            for(int k = 1; k <= sp; k++){
+                    System.out.print("\t");
+            }
+            for(int k = 1; k <= star; k++){
+                    System.out.print("*\t");
+            }
+            
             if(i <= n/2){
-                space--;
-                star += 2;
+               star--;
+               sp += 2;
             }
             else{
-                space++;
-                star -= 2;
+                star++;
+                sp -= 2;
             }
             System.out.println();
         }
